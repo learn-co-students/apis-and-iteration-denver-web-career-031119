e1e3d1a3c3ec02e6076faa999cf25d8a04346288
DangerN <cdneal091@gmail.com>
2019-03-12 14:49:17 -0600

    First switch

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -9,6 +9,13 @@ def get_character_movies_from_api(character_name)
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
+  #binding.pry
+  response_hash['results'].map do | character |
+    puts character["name"]
+    return character['films'] if character['name'] == character_name
+    #binding.pry
+  end
+
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
   # return value of this method should be collection of info about each film.
@@ -17,16 +24,18 @@ def get_character_movies_from_api(character_name)
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
 end
-
+#get_character_movies_from_api('C-3PO')
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+  puts films.map { | film | JSON.parse(RestClient.get(film))['title'] }
 end
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
+  binding.pry
   print_movies(films)
 end
-
+#show_character_movies('Luke Skywalker')
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
